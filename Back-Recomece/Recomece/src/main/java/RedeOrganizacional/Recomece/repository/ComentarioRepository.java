1e0aaef6fe1883d4ea0e1cdd1c403418b6ad443e
VitoriaAlric <vitoriacf@hotmail.com>
2021-02-06 19:06:07 -0300

    Integração Back e Front para Deploy

--- Back-Recomece/Recomece/src/main/java/RedeOrganizacional/Recomece/repository/ComentarioRepository.java
+++
@@ -1,9 +0,0 @@
-package RedeOrganizacional.Recomece.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import RedeOrganizacional.Recomece.model.ComentarioModel;
-
-public interface ComentarioRepository extends JpaRepository<ComentarioModel, Long> {
-
-}
